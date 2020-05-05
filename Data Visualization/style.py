21fc84f0df7c0ce4091ea01809c81f654ab07ea9
JulianBons <63979800+JulianBons@users.noreply.github.com>
2020-05-05 09:39:52 +0200

    Add files via upload

---
+++ Data Visualization/style.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  3 13:32:24 2020
+
+@author: julianbons
+"""
+
+
+
+SIDEBAR_STYLE = {
+    'position': 'fixed',
+    'top': 0,
+    'left': 0,
+    'bottom': 0,
+    'width': '16rem',
+    'padding': '2rem 1rem',
+    'background-color': '#fafafa',
+}
+
+
+CONTENT_STYLE = {
+    'margin-left': '16rem',
+    'margin-right': '2rem',
+    'padding': '2rem 1rem',
+    'background-color': 'white' 
+}
+
+FOOTER_STYLE = {
+    'margin-left': '17rem',
+    'height': '2rem',
+    'color': '#686868',
+    'position': 'fixed',
+    'bottom': 0,
+    'text-align': 'center',
+}
+
